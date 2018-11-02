3a7ce27e34de7a8130eeab8d00e23d8dec80fc3e
71e6fd52 <71e6fd52@gmail.com>
2018-11-02 11:37:59 +0800

    minus

--- button.rb
+++ button.rb
@@ -23,3 +23,21 @@ class Plus
     now + num
   end
 end
+
+class Minus
+  attr_accessor :number
+
+  def self.parse(str)
+    m = str.match(/^-(\d+)$/)
+    raise 'Not Minus' unless m
+    new(m[1].to_i)
+  end
+
+  def initialize(num)
+    @number = num
+  end
+
+  def click(now)
+    now - num
+  end
+end
