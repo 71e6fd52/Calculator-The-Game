2a279bc6e934aa8f6e15137f945a601552289ffd
71e6fd52 <71e6fd52@gmail.com>
2018-11-02 11:27:46 +0800

    plus

---
+++ button.rb
@@ -0,0 +1,25 @@
+class Button
+  def class(str)
+    case str
+    when /^+/ then Plus
+    end
+  end
+end
+
+class Plus
+  attr_accessor :number
+
+  def self.parse(str)
+    m = str.match(/^+(\d+)$/)
+    raise 'Not Plus' unless m
+    new(m[1].to_i)
+  end
+
+  def initialize(num)
+    @number = num
+  end
+
+  def click(now)
+    now + num
+  end
+end
